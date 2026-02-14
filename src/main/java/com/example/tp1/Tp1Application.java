398855df798a061d9c3ccd3b15ed8344ed6255d1
ayoub timoumi <ayoubtimoumi01@gmail.com>
2026-02-14 23:28:16 +0100

    removed one of the two cpmmandLiners

--- src/main/java/com/example/tp1/Tp1Application.java
+++ src/main/java/com/example/tp1/Tp1Application.java
@@ -1,13 +1,25 @@
 package com.example.tp1;
 
+import com.example.tp1.entities.Etudiant;
+import com.example.tp1.repositories.EtudiantRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDate;
+
 @SpringBootApplication
-public class Tp1Application {
+public class Tp1Application implements CommandLineRunner {
+    public Tp1Application(EtudiantRepository etudiantRepository) {
+        this.etudiantRepository = etudiantRepository;
+    }
+    EtudiantRepository etudiantRepository;
+    public static void main(String[] args) {
+        SpringApplication.run(Tp1Application.class, args);
+    }
+    @Override
+    public void run(String... args) throws Exception {
+        etudiantRepository.save(new Etudiant("Juanito", "Maria", LocalDate.of(1980, 1, 1)));
 
-	public static void main(String[] args) {
-		SpringApplication.run(Tp1Application.class, args);
-	}
-
+    }
 }
